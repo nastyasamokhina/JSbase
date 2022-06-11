2b282973b5fd9166c89c9b439da7324285921906
Nastya Samokhina <nasty.samohina@gmail.com>
2022-06-11 21:59:27 +0300

    11/06

--- basket.js
+++ basket.js
@@ -1,1 +1,24 @@
 'use strict';
+
+/*Поставил в js обработчик на открытие/закрытие окна корзины (ставлю/снимаю)
+класс hidden у элемента с классом basket при клике на элемент с классом
+cartIconWrap.
+*/
+
+const cartIconWrapEl = document.querySelector('.cartIconWrap');
+const basketEl = document.querySelector('.basket');
+
+cartIconWrapEl.addEventListener('click', () => {
+  basketEl.classList.toggle('hidden');
+});
+
+let basketObject = {
+  1: {
+
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+}
