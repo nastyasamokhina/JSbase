af760b19d385ac4908ad184336367bfaea0720b4
Nastya Samokhina <nasty.samohina@gmail.com>
2022-06-13 23:03:58 +0300

    13.06

--- basket.js
+++ basket.js
@@ -12,13 +12,50 @@ cartIconWrapEl.addEventListener('click', () => {
   basketEl.classList.toggle('hidden');
 });
 
-let basketObject = {
-  1: {
+let basket = {};
 
-  },
-  2: {},
-  3: {},
-  4: {},
-  5: {},
-  6: {},
-}
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.count = 1;
+  }
+  addItem() {
+    this.count++;
+  }
+
+  getProductMarkup() {
+  return `
+    <div class="basketRow">
+        <div>${this.name}</div>
+        <div>${this.count}</div>
+        <div>${this.price}</div>
+        <div>${this.count * this.price}</div>
+    </div>
+  `;
+  }
+};
+
+const productsEl = document.querySelector('.featuredItems');
+const productEl = document.querySelectorAll('.featuredItem');
+productsEl.addEventListener('click', event => {
+  if (!event.target.classList.contains('addToCart')) {
+    return;
+  } else {
+    addToCart(event.target);
+  }
+ });
+
+function addToCart(target) {
+  let id = target.closest('.featuredItem').dataset.id;
+  let name = target.closest('.featuredItem').dataset.name;
+  let price = target.closest('.featuredItem').dataset.price;
+  if (id in basket) {
+    basket[id].addItem();
+  } else {
+    basket[id] = new Product(name, price);
+    }
+  for (let key in basket) {
+  console.log(basket[key]);
+  }
+};
